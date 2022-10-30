2a1bfaad72ae70dfbe21592f9533ecdaa59b38a4
sobitd <sobitdaniel@gmail.com>
2022-10-30 12:58:26 +0300

    add serialized routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    user = User.find_by(id: session[:user_id])
+    user = User.find_by(params[:id])
     render json: user, include: :parcels, status: :ok
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:create]
+  resources :users, only: [:create,:show, :index]
   resources :parcels
 end
