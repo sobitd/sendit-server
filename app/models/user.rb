d9550f16e632fef9836e8eaee2cd7522747f9386
sobitd <sobitdaniel@gmail.com>
2022-10-30 14:42:26 +0300

    remove unwanted validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,6 @@ class User < ApplicationRecord
   # belongs_to :order, optional: true
 
   validates_presence_of :first_name, :last_name, :phone_number, :username, :password
-  validates :username, uniqueness: true
-  validates :password, length: { in: 6..20 }
   validates :email_address, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
   number_regex = /\d[0-9]\)*\z/
   validates_format_of :phone_number, with: number_regex
