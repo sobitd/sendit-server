5b3aa0aa502c8ed13554f46a11cf20f3c6385470
sobitd <sobitdaniel@gmail.com>
2022-10-30 13:22:02 +0300

    add serializers

--- app/controllers/parcels_controller.rb
+++ app/controllers/parcels_controller.rb
@@ -1,42 +1,43 @@
 class ParcelsController < ApplicationController
-    def index 
-        render json: Parcel.all, status: :ok
-    end
+  def index
+    render json: Parcel.all, status: :ok
+  end
 
-    def show 
-        render json: finder, status: :ok
-    end
+  def show
+    render json: finder, status: :ok
+  end
 
-    def create 
-        parcel = Parcel.create(params)
-        render json: parcel, status: :created
-    end
+  def create
+    parcel = Parcel.create(params)
+    render json: parcel, status: :created
+  end
 
-    def update 
-        parcel = finder 
-        parcel.update(params)
-        render json:parcel, status: :ok
-    end
+  def update
+    parcel = finder
+    parcel.update(params)
+    render json: parcel, status: :ok
+  end
 
-    def destroy
-        find_params.destroy
-        head :no_content
-    end
+  def destroy
+    find_params.destroy
+    head :no_content
+  end
 
-    private 
+  private
 
-    def finder
-        Parcel.find(params[:id])
-      end
-    
-      def params
-        params.permit(
-          :recipient_name,
-          :recipient_address,
-          :recipient_contact,
-          :weight,
-          :from,
-          :destination,
-          :user_id
-        )
+  def finder
+    Parcel.find(params[:id])
+  end
+
+  def params
+    params.permit(
+      :recipient_name,
+      :recipient_address,
+      :recipient_contact,
+      :weight,
+      :from,
+      :destination,
+      :user_id
+    )
+  end
 end
